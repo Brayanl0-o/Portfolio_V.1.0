90dedb55aa3f420f72ed11412f1ec53c6d5d4355
Brayan <brayantandap@gmail.com>
2024-02-24 14:36:18 -0500

    updated > footer.component.ts

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ContactService } from 'src/app/services/contact.service';
 
@@ -9,6 +9,7 @@ import { ContactService } from 'src/app/services/contact.service';
 })
 export class FooterComponent {
   constructor(private contactService: ContactService,
+    private renderer: Renderer2,
     private fb:FormBuilder){}
 
   contactForm!: FormGroup;
@@ -17,52 +18,12 @@ export class FooterComponent {
   showNotAvaible: boolean = false
   sendSuccess:boolean = false;
 
-  ngOnInit(){
-    this.contactForm = this.initForm();
-  }
-  onFormSubmit(){
-    if (this.contactForm.valid) {
-      this.sendForm();
-    } else {
-      this.errorResponseMessageForm = 'Verifique el formulario!';
-        setTimeout(() => {
-          this.errorResponseMessageForm = '';
-        }, 5000);
-    }
-  }
-  sendForm(){
-    if(this.contactForm.valid){
-      const emailData = this.contactForm.value;
-
-      this.contactService.sendInfoForm(emailData).subscribe((response) =>{
-        this.showContactForm = false;
-        this.sendSuccess = true;
-      },
-      (error)=> {
-        console.error('Error send form', error);
-      }
-      )
-    }
-  }
-  initForm(): FormGroup{
-    return this.fb.group({
-      sendersName: ['', [Validators.required, Validators.minLength(4),Validators.maxLength(80)]],
-      emailAddress:['', [Validators.required, Validators.minLength(8),Validators.maxLength(90)]],
-      // phoneNumber:['', [Validators.maxLength(20)]],
-      message:['', [Validators.required, Validators.minLength(8),Validators.maxLength(600)]],
-    })
-  }
-
   toggleContactForm(){
     this.showContactForm = !this.showContactForm;
+    // this.renderer.setStyle(document.body, 'overflow', 'hidden');
+  }
+  ngOnInit(){
+    this.contactService.$modal.subscribe((valu) => { this.showContactForm =valu })
   }
 
-  toggleShowNotAvaible(){
-    this.showNotAvaible = !this.showNotAvaible;
-    if (this.showNotAvaible) {
-      setTimeout(() => {
-        this.showNotAvaible = false;
-      }, 3000);
-    }
-  }
 }
