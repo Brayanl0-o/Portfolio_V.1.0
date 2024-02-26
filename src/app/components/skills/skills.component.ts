176356d83007be4dc2670245e88d158d0ba46682
Brayan <brayantandap@gmail.com>
2024-02-26 02:01:03 -0500

    updated > skills.component

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.css']
+  styleUrls: ['./skills.component.css'],
+  // encapsulation: ViewEncapsulation.Emulated
+
 })
 export class SkillsComponent {
   showBackend: boolean = false;
