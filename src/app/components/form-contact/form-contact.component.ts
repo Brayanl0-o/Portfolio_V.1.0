8f7ca1a3667c3e62624eb4d438b4cb12f9be12d2
Brayan <brayantandap@gmail.com>
2024-02-26 02:01:35 -0500

    updated > form-contact.component.ts

--- src/app/components/form-contact/form-contact.component.ts
+++ src/app/components/form-contact/form-contact.component.ts
@@ -1,16 +1,20 @@
-import {  Component, Renderer2 } from '@angular/core';
+import {  Component, Renderer2, ViewEncapsulation} from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar, MatSnackBarConfig, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
 import { ContactService } from 'src/app/services/contact.service';
-
+import { CustomSnackbarComponent } from '../custom-snackbar/custom-snackbar.component';
 @Component({
   selector: 'app-form-contact',
   templateUrl: './form-contact.component.html',
-  styleUrls: ['./form-contact.component.css']
+  styleUrls: ['./form-contact.component.css'],
+  encapsulation: ViewEncapsulation.None
+
 })
 export class FormContactComponent {
   constructor(private contactService: ContactService,
     private renderer: Renderer2,
-    private fb:FormBuilder){}
+    private fb:FormBuilder,
+    private snackBar: MatSnackBar){}
 
     contactForm!: FormGroup;
     errorResponseMessageForm: string | null = null;
@@ -19,15 +23,28 @@ export class FormContactComponent {
     showDetailsProject: boolean =  false;
     isLoading: boolean = false;
 
-    toggleShowDetails(){
-      this.showDetailsProject = !this.showDetailsProject;
-    }
+    openSnackBar(){
+      const config = new MatSnackBarConfig();
+      config.duration = 8000;
+      config.horizontalPosition = 'center';
+      config.verticalPosition = 'top'; // Posición en la parte superior
+      config.panelClass = ['custom-snackbar-slider'];
 
-    closeModal() {
-      // this.renderer.removeStyle(document.body, 'overflow');
-      this.contactService.$modal.emit(false)
+      // Personalización del estilo del texto
+      config.data = {
+        message: '¡Formulario enviado con éxito! Pronto me contactaré contigo.',
+        fontSize: '20px',
+        fontWeight: 'bold',
+        color: '#000231',
+      };
+      this.snackBar.openFromComponent(CustomSnackbarComponent, {
+        data: config.data, // Pasa los datos al componente del snackbar
+        duration: config.duration, // Duración del snackbar
+        horizontalPosition: config.horizontalPosition, // Posición horizontal
+        verticalPosition: config.verticalPosition, // Posición vertical
+        panelClass: config.panelClass // Clase de estilo personalizado
+      });
     }
-
     ngOnInit(){
       this.contactForm = this.initForm();
     }
@@ -41,18 +58,19 @@ export class FormContactComponent {
           this.closeModal()
           this.isLoading = false;
 
-          this.contactService.$success_send.emit(true)
-          setTimeout(() => {
-            this.contactService.$success_send.emit(false);
-          }, 5000);
+          // this.contactService.$success_send.emit(true)
+          // setTimeout(() => {
+          //   this.contactService.$success_send.emit(false);
+          // }, 5000);
         },
         (err)=> {
           console.error('Error send form', err);
+          this.errorResponseMessageForm = '¡Error del servidor! Vuelve a intentarlo mas tarde.';
           this.isLoading = false;
 
         })
       }else{
-        this.errorResponseMessageForm = 'Falta un campo!';
+        this.errorResponseMessageForm = '¡Falta un campo!';
         this.isLoading = false;
 
         setTimeout(() => {
@@ -68,18 +86,13 @@ export class FormContactComponent {
         message:['', [Validators.required, Validators.minLength(8),Validators.maxLength(600),Validators.pattern('[A-Za-z\\s]+')]],
       })
     }
+    toggleShowDetails(){
+      this.showDetailsProject = !this.showDetailsProject;
+    }
+    closeModal() {
+      // this.renderer.removeStyle(document.body, 'overflow');
+      this.contactService.$modal.emit(false)
+    }
 
 }
-  // onFormSubmit(){
-    //   this.isLoading = true;
 
-    //   if (this.contactForm.valid) {
-    //     this.sendForm();
-
-    //   } else {
-    //     this.errorResponseMessageForm = 'Verifique el formulario!';
-    //       setTimeout(() => {
-    //         this.errorResponseMessageForm = '';
-    //       }, 5000);
-    //   }
-    // }
