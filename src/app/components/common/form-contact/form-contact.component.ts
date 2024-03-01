652bd07fec7a61d0b9c36b72f254d008e2e2f4c2
Brayan <brayantandap@gmail.com>
2024-03-01 01:08:28 -0500

    updated > form-contact.component

--- src/app/components/common/form-contact/form-contact.component.ts
+++ src/app/components/common/form-contact/form-contact.component.ts
@@ -3,6 +3,8 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar, MatSnackBarConfig, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
 import { ContactService } from 'src/app/services/contact.service';
 import { CustomSnackbarComponent } from '../custom-snackbar/custom-snackbar.component';
+import { AlertStates } from '../../../models/form'
+
 @Component({
   selector: 'app-form-contact',
   templateUrl: './form-contact.component.html',
@@ -12,23 +14,65 @@ import { CustomSnackbarComponent } from '../custom-snackbar/custom-snackbar.comp
 })
 export class FormContactComponent {
   constructor(private contactService: ContactService,
-    private renderer: Renderer2,
     private fb:FormBuilder,
     private snackBar: MatSnackBar){}
 
     contactForm!: FormGroup;
     errorResponseMessageForm: string | null = null;
     showContactForm: boolean = false;
-    showNotAvaible: boolean = false
     showDetailsProject: boolean =  false;
     isLoading: boolean = false;
     messageAlert: boolean = false;
 
+    alertStates: AlertStates = {
+      sendersName: false,
+      emailAddress:false,
+    }
+
+    toggleAlertState(field: keyof AlertStates){
+      this.alertStates[field] = !this.alertStates[field];
+    }
     showMessageAlert(){
         this.messageAlert = true;
     }
     hiddeMessageAlert(){
       this.messageAlert = false;
+    }
+    ngOnInit(){
+      this.contactForm = this.initForm();
+    }
+    sendForm(){
+      this.isLoading = true;
+      if(this.contactForm.valid){
+        const emailData = this.contactForm.value;
+        this.contactService.sendInfoForm(emailData).subscribe((response) =>{
+          this.closeModal()
+          this.isLoading = false;
+          this.openSnackBar()
+        },
+        (err)=> {
+          console.error('Error send form', err);
+          this.errorResponseMessageForm = '¡Error del servidor! \n Vuelve a intentarlo mas tarde.';
+          this.isLoading = false;
+          setTimeout(() => {
+            this.errorResponseMessageForm = null;
+          }, 5000);
+        })
+      }else{
+        this.errorResponseMessageForm = '¡Falta un campo!';
+        this.isLoading = false;
+        setTimeout(() => {
+          this.errorResponseMessageForm = null;
+        }, 5000);
+      }
+    }
+
+    initForm(): FormGroup{
+      return this.fb.group({
+        sendersName: ['', [Validators.required, Validators.minLength(4),Validators.maxLength(80),Validators.pattern('[A-Za-z\\s]+')]],
+        emailAddress:['', [Validators.required, Validators.minLength(8),Validators.maxLength(90),Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)]],
+        message:['', [Validators.required, Validators.minLength(8),Validators.maxLength(600),Validators.pattern('[A-Za-z\\s]+')]],
+      })
     }
     openSnackBar(){
       const config = new MatSnackBarConfig();
@@ -47,56 +91,10 @@ export class FormContactComponent {
         verticalPosition: config.verticalPosition,
       });
     }
-    ngOnInit(){
-      this.contactForm = this.initForm();
-    }
-
-    sendForm(){
-      this.isLoading = true;
-
-      if(this.contactForm.valid){
-        const emailData = this.contactForm.value;
-        this.contactService.sendInfoForm(emailData).subscribe((response) =>{
-          this.closeModal()
-          this.isLoading = false;
-          this.openSnackBar()
-          // this.contactService.$success_send.emit(true)
-          // setTimeout(() => {
-          //   this.contactService.$success_send.emit(false);
-          // }, 5000);
-        },
-        (err)=> {
-          console.error('Error send form', err);
-          this.errorResponseMessageForm = '¡Error del servidor! \n Vuelve a intentarlo mas tarde.';
-          this.isLoading = false;
-          setTimeout(() => {
-            this.errorResponseMessageForm = null;
-          }, 5000);
-
-
-        })
-      }else{
-        this.errorResponseMessageForm = '¡Falta un campo!';
-        this.isLoading = false;
-
-        setTimeout(() => {
-          this.errorResponseMessageForm = null;
-        }, 5000);
-
-      }
-    }
-    initForm(): FormGroup{
-      return this.fb.group({
-        sendersName: ['', [Validators.required, Validators.minLength(4),Validators.maxLength(80),Validators.pattern('[A-Za-z\\s]+')]],
-        emailAddress:['', [Validators.required, Validators.minLength(8),Validators.maxLength(90),Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)]],
-        message:['', [Validators.required, Validators.minLength(8),Validators.maxLength(600),Validators.pattern('[A-Za-z\\s]+')]],
-      })
-    }
     toggleShowDetails(){
       this.showDetailsProject = !this.showDetailsProject;
     }
     closeModal() {
-      // this.renderer.removeStyle(document.body, 'overflow');
       this.contactService.$modal.emit(false)
     }
 
