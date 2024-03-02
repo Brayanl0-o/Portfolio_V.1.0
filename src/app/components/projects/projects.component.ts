016b2f338fb1c30985440a5a62112b80a5c773d0
Brayan <brayantandap@gmail.com>
2024-03-02 11:49:39 -0500

    updated > projects.component

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,55 +1,14 @@
-import { Component, ElementRef, HostListener } from '@angular/core';
-import { trigger, state, style, animate, transition } from '@angular/animations';
+import { Component } from '@angular/core';
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.css'],
-  animations: [
-    trigger('aparicion-titles-projects',[
-      state('hidden', style({
-        opacity: 0,
-        transform: 'scale(0.6)'
-      })),
-      state('visible', style({
-        opacity: 1,
-        transform: 'scale(1)'
-      })),
-      transition('hidden => visible', animate('2.5s ease-in')),
-    ]),
-    trigger('aparicion-images-projects', [
-      state('hidden', style({
-        opacity: 0.2,
-        transform: 'scale(0.7)'
-      })),
-      state('visible', style({
-        opacity: 1,
-        transform: 'scale(1)'
-      })),
-      transition('hidden => visible', animate('1.5s ease-in')),
-    ]),
-  ]
+  styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent {
   state_projects = 'hidden';
-  // sectionOffsetTop = 0;
-  // constructor(private el: ElementRef) {}
-
-  // ngOnInit() {
-  //   this.sectionOffsetTop = this.el.nativeElement.offsetTop;
-  // }
-
   showDetailsProject: boolean =  false;
 
   toggleShowDetails(){
     this.showDetailsProject = !this.showDetailsProject;
   }
-
-  // @HostListener('window:scroll', ['$event'])
-  // checkScroll() {
-  //   const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-  //   const scrollThreshold = 50;
-  //   if (scrollPosition > scrollThreshold) {
-  //     this.state_projects = 'visible';
-  //   }
-  // }
 }
